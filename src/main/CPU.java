bdcac9a4ca8ee7a8455fd99ef4fa89a9f1241dda
juangil991 <jcgo125@gmail.com>
2022-02-09 18:45:47 -0500

    se añade la funcion MOV

--- src/main/CPU.java
+++ src/main/CPU.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 public class CPU {
     public static HashMap<String, Integer> registros = new HashMap<String, Integer>();
 
+    public static void funcionesCPU(String[] subrutina) {
+
         registros.put("R00", 0);
         registros.put("R01", 0);
         registros.put("R02", 0);
@@ -51,41 +53,74 @@ public class CPU {
         registros.put("R41", 0);
         registros.put("R42", 0);
 
-    public static void funcionesCPU(String[] subrutina) {
-
-
-
-
-        //String mensaje = "";
         int index = 0;
-        //List<String> Instrucciones = List.of(suprutina);
-        //int NoIns = Instrucciones.size();
+        List<String> Instrucciones = List.of(subrutina);
+        int NoIns = Instrucciones.size();
 
-        for (String instruccion:subrutina){
-            subrutina[0]="MOV 5,R01";
-            String accion=instruccion.split(" ")[0];
-            String[] valores = new String [2];
-            valores[0]=instruccion.split(" ")[1].split(",")[0];//Primer numero o registro
-            valores[1]=instruccion.split(" ")[1].split(",")[1];//Segundo registro
-
-                    if(accion=="MOV"){
-                        //if(Integer.parseInt(valores[0])){
-                            CPU.registros.put(valores[1],Integer.parseInt(valores[0]));
-
-
-                        //}
-
-                    }
-
+        while(index<NoIns)
+        {
+            if(Instrucciones.get(index).startsWith("MOV R"))
+            {
+                System.out.println("se entro a la instruccion MOV Rxx");
+                index++;
+                continue;
+            }
+            if(Instrucciones.get(index).startsWith("MOV"))
+            {
+                String accion=subrutina[index].split(" ")[0];
+                String[] valores = subrutina[index].split(" ")[1].split(",");
+                //valores[0]=instruccion.split(" ")[1].split(",")[0];//Primer numero o registro
+                //valores[1]=instruccion.split(" ")[1].split(",")[1];//Segundo registro
+                //Hola, puedes oirme?
+                // if(accion=="MOV"){
+                //if(Integer.parseInt(valores[0])){
+                CPU.registros.put(valores[1],Integer.parseInt(valores[0]));
+                System.out.println("se entro a la instruccion MOV d");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("ADD"))
+            {
+                System.out.println("se entro a la instruccion ADD");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("DEC"))
+            {
+                System.out.println("se entro a la instruccion DEC");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("INC R"))
+            {
+                System.out.println("se entro a la instruccion INC");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("INV R"))
+            {
+                System.out.println("se entro a la instruccion INV");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("JMP"))
+            {
+                System.out.println("se entro a la instruccion JMP");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("JZ"))
+            {
+                System.out.println("se entro a la instruccion JZ");
+                index++;
+            }
+            if(Instrucciones.get(index).startsWith("NOP"))
+            {
+                System.out.println("se entro a la instruccion NOP");
+                index++;
+            }
         }
-
     }
-
     public static void main(String[] args) {
         int[] registro = new int[42];
-        String[] subrutina = {"MOV 5, R01", "MOV 10,R01", "JZ 7", "ADD R02,R01", "DEC R00", "JMP 3", "MOV R02,R42"};
+        String[] subrutina = {"MOV 5,R01", "MOV 10,R02", "JZ 7", "ADD R02,R01", "DEC R00", "JMP 3", "MOV R02,R42"};
         CPU.funcionesCPU(subrutina);
         System.out.println(CPU.registros.get("R01"));
+        System.out.println(CPU.registros.get("R02"));
     }
 
 
