d39d7303b4fe4e98f4a6843359cce4d4219b0e50
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-02-09 19:41:48 -0500

    Actualizacion ultimos metodos

--- src/main/logica.java
+++ src/main/logica.java
@@ -10,11 +10,15 @@ public class logica {
         List<Registro> lista = new ArrayList<>();
         Registro R01 = new Registro("R01", 0);
         lista.add(R01);
-        Registro R02 = new Registro("R02", 1010);
+        Registro R02 = new Registro("R02", 1);
         lista.add(R02);
         //ADD_calculaMood(R01, R02);
-        //System.out.println(DEC_decrementa(R01));
-        INV_inversionBitaBit(R02);
+        int y = Integer.reverse(1);
+        System.out.println(y);
+
+        double x = Math.pow(2, 32)-1;
+        System.out.println(x);
+
 
 
     }
@@ -38,7 +42,7 @@ public class logica {
     // TODO: 9/02/22 pendiente de revision
     public static int DEC_decrementa(Registro rxx){
         if(rxx.valor == 0){
-            double aux = Math.pow(2.0,32)-1;
+            double aux = Math.pow(2,32)-1;
             rxx.valor = (int)aux;
 
         }else{
@@ -60,17 +64,16 @@ public class logica {
     }
 
     // TODO: 9/02/22 pendiente de revision
-    public static void INV_inversionBitaBit(Registro rxx){
+    public static int INV_inversionBitaBit(Registro rxx){
+        /*String cadena;
+        cadena = String.valueOf(rxx.valor);
+        rxx.valor = (Integer)rxx.valor;*/
 
-        //String cadena;
-        //cadena = String.valueOf(rxx.valor);
-       // rxx.valor = (Integer)rxx.valor;
-        Integer i = Integer.valueOf(rxx.valor);
-        i.reverse(rxx.valor);
-        System.out.println(rxx.valor);
+        int x =Integer.reverse(rxx.valor);
+        return x;
 
     }
 
-    public static void Nop(){}
+    public static void NOP(){}
 
 }
